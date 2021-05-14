ad9a10aa59bdd4b476ef3c4562bcd4b71936eae6
Travis Pettry <travis.pettry@gmail.com>
2021-05-14 13:34:24 -0400

    Added The Modal panel

---
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/Modal/Modal.razor.cs
@@ -0,0 +1,37 @@
+﻿using AppsByTAP.BlazorFluentUI.Components.BaseComponent;
+using Microsoft.AspNetCore.Components;
+
+namespace AppsByTAP.BlazorFluentUI.Components.Modal
+{
+    public class ModalViewModel : BaseComponentViewModel
+    {
+        private bool _showWindow;
+        [Parameter]
+        public bool ShowWindow
+        {
+            get => _showWindow;
+            set
+            {
+                _showWindow = value;
+                _displayType = value ? "block" : "none";
+            }
+        }
+
+        [Parameter]
+        public EventCallback<bool> ShowWindowChanged { get; set; }
+
+        [Parameter]
+        public RenderFragment Content { get; set; }
+        [Parameter]
+        public RenderFragment Header { get; set; }
+
+
+        protected string _displayType;
+
+        protected async void Close()
+        {
+            ShowWindow = false;
+            await ShowWindowChanged.InvokeAsync(false);
+        }
+    }
+}
