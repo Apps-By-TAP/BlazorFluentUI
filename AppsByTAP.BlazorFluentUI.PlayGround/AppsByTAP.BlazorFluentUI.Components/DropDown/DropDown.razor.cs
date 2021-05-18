dc969a1fb2b12f9f7cb7bbb391de0ff94d46595b
Travis Pettry <travis.pettry@gmail.com>
2021-05-18 06:41:31 -0400

    started to add multi select to the dropdown

--- AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/DropDown/DropDown.razor.cs
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/DropDown/DropDown.razor.cs
@@ -2,14 +2,33 @@
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AppsByTAP.BlazorFluentUI.Components.DropDown
 {
     public class DropDownViewModel<T> : BaseComponentViewModel
     {
+        private T _selectedItem;
         [Parameter]
-        public T SelectedItem { get; set; }
+        public T SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                if(_selectedItem is not null && _selectedItem.Equals(value)) { return; }
+
+                _selectedItem = value;
+
+                if(value is not null)
+                {
+                    _selectedDisplayText = value.ToString();
+                }
+
+                _displayDropDown = false;
+                Task.Run(async () => await SelectedItemChanged.InvokeAsync(value));
+            }
+        }
 
         [Parameter]
         public EventCallback<T> SelectedItemChanged { get; set; }
@@ -19,8 +38,39 @@ namespace AppsByTAP.BlazorFluentUI.Components.DropDown
 
         [Parameter]
         public bool Disabled { get; set; }
+        [Parameter]
+        public bool IsMultiSelect { get; set; }
+
+        private IList<T> _selectedItems;
+        [Parameter]
+        public IList<T> SelectedItems
+        {
+            get => _selectedItems;
+            set
+            {
+                if (_selectedItems is not null && _selectedItems.Equals(value)) { return; }
+
+                _selectedItems = value;
+
+                if(SelectedItems.Count > 0)
+                {
+                    _selectedDisplayText = SelectedItems.Select(x => x.ToString()).Aggregate((x, y) => $"{x}, {y}");
+                }
+
+                Task.Run(async () => await SelectedItemsChanged.InvokeAsync(value));
+            }
+        }
+
+        [Parameter]
+        public EventCallback<IList<T>> SelectedItemsChanged { get; set; }
 
         protected bool _displayDropDown = false;
+        protected string _selectedDisplayText { get; set; } = "Select an option";
+
+        public DropDownViewModel() : base()
+        {
+            SelectedItems = new List<T>();
+        }
 
         protected void OpenDropDown()
         {
@@ -38,8 +88,6 @@ namespace AppsByTAP.BlazorFluentUI.Components.DropDown
         protected async Task SelectItem(T selectedItem)
         {
             SelectedItem = selectedItem;
-            _displayDropDown = false;
-            await SelectedItemChanged.InvokeAsync(selectedItem);
         }
     }
 }
