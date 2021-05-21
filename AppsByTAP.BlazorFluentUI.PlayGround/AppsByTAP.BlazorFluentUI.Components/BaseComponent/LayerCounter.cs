bc9a473e785e626e33efacc2c3b61dec4b9de5c7
Travis Pettry <travis.pettry@gmail.com>
2021-05-21 08:15:46 -0400

    Added a hack for layering.

---
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/BaseComponent/LayerCounter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppsByTAP.BlazorFluentUI.Components.BaseComponent
+{
+    /// <summary>
+    /// This is a hack for layering. Will redo this in time.
+    /// </summary>
+    public static class LayerCounter
+    {
+        private static int _layer = 1000000;
+
+        public static int GetLayer() => _layer++;
+    }
+}
