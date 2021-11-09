f9ec0649e9723561a42e157ac6975aee7f751c64
Travis Pettry <travis.pettry@gmail.com>
2021-11-09 08:05:15 -0500

    Tab Control with motion.

---
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/Tabs/Tab.razor.cs
@@ -0,0 +1,13 @@
+﻿//using AppsByTAP.BlazorFluentUI.Components.BaseComponent;
+//using Microsoft.AspNetCore.Components;
+
+//namespace AppsByTAP.BlazorFluentUI.Components.Tabs
+//{
+//    public partial class Tab : ComponentBase
+//    {
+//        [Parameter]
+//        public string Header { get; set; }
+//        [Parameter]
+//        public RenderFragment Content { get; set; }
+//    }
+//}
---
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/Tabs/Tabs.razor.cs
@@ -0,0 +1,19 @@
+﻿//using AppsByTAP.BlazorFluentUI.Components.BaseComponent;
+//using Microsoft.AspNetCore.Components;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace AppsByTAP.BlazorFluentUI.Components.Tabs
+//{
+//    public partial class Tabs : ComponentBase
+//    {
+//        [Parameter]
+//        public List<Tab> Content { get; set; } = new List<Tab>();
+
+//        [Parameter]
+//        public List<RenderFragment> ItemTemplates { get; set; }
+//    }
+//}
