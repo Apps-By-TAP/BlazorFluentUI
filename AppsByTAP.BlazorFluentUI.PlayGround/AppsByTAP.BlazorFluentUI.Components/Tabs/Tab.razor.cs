eb5e3de45cdfd31fd72ff3b7ab0b955b18ee40bc
Travis Pettry <travis.pettry@gmail.com>
2021-11-11 07:52:05 -0500

    Fixed animation, made async for wasm. but now you have to define a height.

--- AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/Tabs/Tab.razor.cs
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/Tabs/Tab.razor.cs
@@ -1,13 +1,58 @@
-﻿//using AppsByTAP.BlazorFluentUI.Components.BaseComponent;
-//using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
 
-//namespace AppsByTAP.BlazorFluentUI.Components.Tabs
-//{
-//    public partial class Tab : ComponentBase
-//    {
-//        [Parameter]
-//        public string Header { get; set; }
-//        [Parameter]
-//        public RenderFragment Content { get; set; }
-//    }
-//}
+namespace AppsByTAP.BlazorFluentUI.Components.Tabs
+{
+    public partial class Tab : ComponentBase
+    {
+        [CascadingParameter]
+        private Tabs Parent { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public string Header { get; set; }
+
+        [Parameter]
+        public string Color { get; set; } = "var(--semanticColors-ButtonText)";
+
+        private string _viewState;
+        private double _left = -40;
+        private int _opacity;
+
+        protected override void OnInitialized()
+        {
+            if (Parent == null)
+                throw new ArgumentNullException(nameof(Parent), "TabPage must exist within a TabControl");
+            base.OnInitialized();
+
+            Parent.SelectionChanged += Parent_SelectionChanged;
+            Parent.AddPage(this);
+        }
+
+        private async void Parent_SelectionChanged()
+        {
+            if(Parent.LastTab == this)
+            {
+                //_viewState = await Delay();
+                _opacity = 0;
+                _left = -40;
+            }
+            else
+            {
+                //_viewState = "block";
+                await Task.Delay(100);
+                _opacity = 1;
+                _left = 0;
+            }
+            StateHasChanged();
+        }
+
+        private async Task<string> Delay()
+        {
+            await Task.Delay(300);
+            return "none";
+        }
+    }
+}
--- AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/Tabs/Tabs.razor.cs
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/Tabs/Tabs.razor.cs
@@ -1,19 +1,70 @@
-﻿//using AppsByTAP.BlazorFluentUI.Components.BaseComponent;
-//using Microsoft.AspNetCore.Components;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using AppsByTAP.BlazorFluentUI.Components.BaseComponent;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace AppsByTAP.BlazorFluentUI.Components.Tabs
-//{
-//    public partial class Tabs : ComponentBase
-//    {
-//        [Parameter]
-//        public List<Tab> Content { get; set; } = new List<Tab>();
+namespace AppsByTAP.BlazorFluentUI.Components.Tabs
+{
+    public partial class Tabs : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public string Width { get; set; }
+        [Parameter]
+        public string Height { get; set; } = "100%";
 
-//        [Parameter]
-//        public List<RenderFragment> ItemTemplates { get; set; }
-//    }
-//}
+        public event Action SelectionChanged;
+
+        private Tab _activePage;
+        public Tab ActivePage 
+        { 
+            get => _activePage;
+            set
+            {
+                if(_activePage == value) { return; }
+
+                _activePage = value;
+                SelectionChanged?.Invoke();
+                StateHasChanged();
+            }
+        }
+
+        public Tab LastTab;
+
+        private List<Tab> Pages = new List<Tab>();
+
+        private double _left;
+        private int _indictorPosition;
+        private double _percent;
+
+        internal void AddPage(Tab tabPage)
+        {
+            Pages.Add(tabPage);
+            if (Pages.Count == 1)
+                ActivePage = tabPage;
+
+            _percent = (1d / (double)Pages.Count) * 100;
+            _left = _indictorPosition * _percent;
+
+            StateHasChanged();
+        }
+
+        string GetButtonClass(Tab page)
+        {
+            return page == ActivePage ? "btn-primary" : "btn-secondary";
+        }
+
+        void ActivatePage(Tab page)
+        {
+            LastTab = ActivePage;
+            ActivePage = page;
+
+            _indictorPosition = Pages.IndexOf(page);
+            _left = _indictorPosition * _percent;
+        }
+    }
+}
