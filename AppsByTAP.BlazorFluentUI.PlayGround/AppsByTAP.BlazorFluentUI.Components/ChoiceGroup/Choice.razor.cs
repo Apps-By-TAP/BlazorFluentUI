1de652f92eb9476b4282895add7afef4a01a532b
Travis Pettry <travis.pettry@gmail.com>
2021-12-13 05:48:16 -0500

    ChoiceGroup needs a selected value

---
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/ChoiceGroup/Choice.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace AppsByTAP.BlazorFluentUI.Components.ChoiceGroup
+{
+    public partial class Choice : ComponentBase
+    {
+        [CascadingParameter]
+        public ChoiceGroup Parent { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public bool IsSelected { get; set; }
+        [Parameter]
+        public EventCallback<bool> IsSelectedChanged { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (Parent == null)
+                throw new ArgumentNullException(nameof(Parent), "TabPage must exist within a TabControl");
+            base.OnInitialized();
+
+            Parent.SelectionChanged += Parent_SelectionChanged;
+        }
+
+
+        private async void Parent_SelectionChanged()
+        {
+            IsSelected = Parent.SelectedChoice == this;
+            await InvokeAsync(() => StateHasChanged());
+        }
+    }
+}
---
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/ChoiceGroup/ChoiceGroup.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace AppsByTAP.BlazorFluentUI.Components.ChoiceGroup
+{
+    public partial class ChoiceGroup : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        public event Action SelectionChanged;
+
+        public Choice SelectedChoice { get; set; }
+
+        public void ChildSelected(Choice selectedChild)
+        {
+            SelectedChoice = selectedChild;
+            SelectionChanged?.Invoke();
+        }
+    }
+}
