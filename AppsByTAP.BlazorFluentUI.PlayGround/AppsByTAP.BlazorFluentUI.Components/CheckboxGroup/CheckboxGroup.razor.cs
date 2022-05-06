3f1de0e54907cf4a6b3618edc6262cc4fcaf890f
Travis Pettry <travis.pettry@gmail.com>
2022-05-06 07:21:07 -0400

    Added the component CheckboxGroup

---
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/CheckboxGroup/CheckboxGroup.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace AppsByTAP.BlazorFluentUI.Components.CheckboxGroup
+{
+    public partial class CheckboxGroup<T> : ComponentBase
+    {
+        [Parameter]
+        public List<T> ItemsSource { get; set; }
+        [Parameter]
+        public List<T> SelectedItems { get; set; } = new List<T>();
+        [Parameter]
+        public EventCallback<List<T>> SelectedItemsChanged { get; set; }
+        [Parameter]
+        public bool UseToggleSwitches { get; set; }
+        [Parameter]
+        public bool IsVertical { get; set; } = true;
+        [Parameter]
+        public bool WrapItems { get; set; }
+        [Parameter]
+        public string Width { get; set; } = "auto";
+        [Parameter]
+        public string Height { get; set; } = "auto";
+
+        private void CheckChanged(bool isChecked, T value)
+        {
+            if(isChecked)
+            {
+                SelectedItems.Add(value);
+            }
+            else
+            {
+                SelectedItems.Remove(value);
+            }
+
+            SelectedItemsChanged.InvokeAsync(SelectedItems);
+        }
+    }
+}
