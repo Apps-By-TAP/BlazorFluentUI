79ffe118423d056e6236236362ff2647ec546bb7
Travis Pettry <travis.pettry@gmail.com>
2022-05-26 08:04:13 -0400

    Fixed data binding for selected items

--- AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/CheckboxGroup/CheckboxGroup.razor.cs
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/CheckboxGroup/CheckboxGroup.razor.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
 using AppsByTAP.BlazorFluentUI.Components.Common;
+using System.Linq;
 
 namespace AppsByTAP.BlazorFluentUI.Components.CheckboxGroup
 {
     public partial class CheckboxGroup<T> : ComponentBase
     {
+        private List<T> _itemsSource = new List<T>();
         [Parameter]
-        public List<T> ItemsSource { get; set; }
+        public List<T> ItemsSource 
+        {
+            get => _itemsSource;
+            set
+            {
+                if(_itemsSource == value) { return; }
+
+                _itemsSource = value;
+                _internalItemsSource = value.Select(x => new ItemWrapper<T>(x)).ToList();
+            }
+        }
+
+        private List<T> _selectedItems = new List<T>();
         [Parameter]
         public List<T> SelectedItems { get; set; } = new List<T>();
         [Parameter]
@@ -25,18 +39,51 @@ namespace AppsByTAP.BlazorFluentUI.Components.CheckboxGroup
         [Parameter]
         public string Label { get; set; }
 
-        private void CheckChanged(bool isChecked, T value)
+        private List<ItemWrapper<T>> _internalItemsSource = new List<ItemWrapper<T>>();
+
+        protected override void OnParametersSet()
         {
-            if(isChecked)
+            _internalItemsSource.ForEach(x => x.IsSelected = false);
+
+            foreach (T item in SelectedItems)
             {
-                SelectedItems.Add(value);
+                ItemWrapper<T> temp = _internalItemsSource.FirstOrDefault(x => x.Item.Equals(item));
+
+                if(temp is not null)
+                {
+                    temp.IsSelected = true;
+                }
+            }
+        }
+
+        private void CheckChanged(bool isChecked, ItemWrapper<T> value)
+        {
+            List<ItemWrapper<T>> temp = new List<ItemWrapper<T>>(_internalItemsSource);
+
+            if (isChecked)
+            {
+                value.IsSelected = true;
             }
             else
             {
-                SelectedItems.Remove(value);
+                value.IsSelected = false;
             }
 
+            SelectedItems = temp.Where(x => x.IsSelected).Select(x => x.Item).ToList();
+
             SelectedItemsChanged.InvokeAsync(SelectedItems);
+        }
+    }
+
+    class ItemWrapper<T>
+    {
+        public T Item { get; set; }
+        public bool IsSelected { get; set; }
+
+        public ItemWrapper(T item)
+        {
+            Item = item;
+            IsSelected = false;
         }
     }
 }
