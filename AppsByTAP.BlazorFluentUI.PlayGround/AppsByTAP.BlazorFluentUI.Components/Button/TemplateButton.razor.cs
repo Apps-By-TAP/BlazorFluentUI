8b083db225f1516e55092d9057c03df7a3481d7f
Travis Pettry <travis.pettry@gmail.com>
2022-07-30 07:12:47 -0400

    added template button

---
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/Button/TemplateButton.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AppsByTAP.BlazorFluentUI.Components.Button
+{
+    public class TemplateButtonViewModel : ButtonBaseParameters
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public string Border { get; set; } = "1px solid var(--semanticColors-ButtonBorder)";
+        [Parameter]
+        public string BorderRadius { get; set; } = "2px";
+    }
+}
