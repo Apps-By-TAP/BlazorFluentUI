40a3dc3f8175ffe29040aadaa12b443ff3160013
Travis Pettry <travis.pettry@gmail.com>
2022-09-05 06:28:26 -0400

    Inital take at callout much work to do here

---
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/Callout/Callout.razor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AppsByTAP.BlazorFluentUI.Components.Callout
+{
+    public partial class Callout : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public EventCallback OnOpen { get; set; }
+        [Parameter]
+        public EventCallback OnClose { get; set; }
+
+        private bool _isOpen;
+        [Parameter]
+        public bool IsOpen
+        {
+            get => _isOpen;
+            set
+            {
+                if (_isOpen == value || Disabled) { return; }
+
+                _isOpen = value;
+                IsOpenChanged.InvokeAsync(IsOpen);
+
+                if (value)
+                {
+                    OnOpen.InvokeAsync();
+                }
+                else
+                {
+                    OnClose.InvokeAsync();
+                }
+            }
+        }
+        [Parameter]
+        public EventCallback<bool> IsOpenChanged { get; set; }
+        [Parameter]
+        public string Width { get; set; } = "300px";
+        [Parameter]
+        public int ItemsPanelHeight { get; set; } = -1;
+        [Parameter]
+        public bool Disabled { get; set; }
+    }
+}
---
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUi.Formatter/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Hello, World!");
+
+string[] lines = File.ReadAllLines(@"R:\temp\css.txt");
+
+StringBuilder sb = new StringBuilder();
+
+for(int i = 0; i < lines.Length; i +=4)
+{
+    sb.Append(lines[i].Trim() + $" {lines[i+1].Trim()} }}{Environment.NewLine}");
+}
+
+File.WriteAllText(@"R:\temp\css-output.txt", sb.ToString());
+
+Regex rex = new Regex(@"--(?<name>[A-Za-z0-9]+):before { content: ""\\(?<code>[A-Za-z0-9]+)""; }");
+MatchCollection matches = rex.Matches(sb.ToString());
+
+
+StringBuilder sb2 = new StringBuilder();
+StringBuilder sb3 = new StringBuilder();
+
+foreach(Match match in matches)
+{
+    if(match.Success)
+    {
+        sb2.Append("\t\t" + match.Groups["name"].Value + "," + Environment.NewLine);
+        sb3.Append(match.Groups["code"].Value + Environment.NewLine);
+    }
+}
+
+string iconTypes = sb2.ToString();
+string unicodeSelection = sb3.ToString();//https://uifabricicons.azurewebsites.net/
+
+Console.WriteLine(iconTypes);
