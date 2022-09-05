a934e263e954672633ead22ad9f43caafacd4620
Travis Pettry <travis.pettry@gmail.com>
2022-09-05 09:10:29 -0400

    Added the lightdismiss component

---
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/LightDismiss/LightDismiss.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AppsByTAP.BlazorFluentUI.Components.LightDismiss
+{
+    public partial class LightDismiss : ComponentBase
+    {
+        [Parameter]
+        public bool IsOpen { get; set; }
+        [Parameter]
+        public EventCallback<bool> IsOpenChanged { get; set; }
+        [Parameter]
+        public EventCallback OnClose { get; set; }
+
+        protected void Close()
+        {
+            IsOpen = false;
+            OnClose.InvokeAsync();
+        }
+    }
+}
