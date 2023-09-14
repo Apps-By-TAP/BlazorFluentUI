e03bc61be9ae66755b1a2deca2629c3bc7bf2356
Travis Pettry <travis.pettry@gmail.com>
2023-09-13 22:31:57 -0400

    fixed dropdown placement

--- AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/wwwroot/js/BlankDropDown.js
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/wwwroot/js/BlankDropDown.js
@@ -1,13 +1,25 @@
 ﻿
 export function canExpandDown(guid) {
+    //var ele = document.getElementById(guid);
+    //var rect = ele.getBoundingClientRect();
+    //const realHeight = window.innerHeight + window.scrollY;
+
+    //alert(window.innerHeight + " " + window.scrollY);
+    //alert(ele.offsetTop + rect.height + " < " + realHeight);
+    //return (ele.offsetTop + rect.height) < realHeight;
+
+
     var ele = document.getElementById(guid);
     var rect = ele.getBoundingClientRect();
-    const realHeight = window.innerHeight + window.scrollY;
-    return (ele.offsetTop + rect.height) < realHeight;
+    const realHeight = window.innerHeight;
+    var result = (rect.y + rect.height) < realHeight;
+    return result;
 }
 
 export function getNewTopLocation(guid) {
     var ele = document.getElementById(guid);
-
-    return ele.parentNode.offsetTop + 3 - ele.getBoundingClientRect().height;
+    var rect = ele.parentNode.getBoundingClientRect();
+    var top = rect.top + window.scrollY;
+    var result = top + 3 - ele.getBoundingClientRect().height;
+    return result;
 }
