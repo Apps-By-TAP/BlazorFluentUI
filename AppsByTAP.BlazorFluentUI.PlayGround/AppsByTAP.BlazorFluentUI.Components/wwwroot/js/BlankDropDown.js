739dff40623120ebd76bc9dbd0c97b2a529e4241
Travis Pettry <travis.pettry@gmail.com>
2023-09-13 21:32:01 -0400

    fixed dropdown flyout placement

--- AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/wwwroot/js/BlankDropDown.js
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/wwwroot/js/BlankDropDown.js
@@ -2,14 +2,12 @@
 export function canExpandDown(guid) {
     var ele = document.getElementById(guid);
     var rect = ele.getBoundingClientRect();
-    const realHeight = window.innerHeight;
-    return rect.y + rect.height < realHeight;
+    const realHeight = window.innerHeight + window.scrollY;
+    return (ele.offsetTop + rect.height) < realHeight;
 }
 
 export function getNewTopLocation(guid) {
     var ele = document.getElementById(guid);
-    var parentRect = ele.parentNode.offsetTop;
-    var eleRect = ele.getBoundingClientRect();
 
-    return parentRect + 3 - eleRect.height;
+    return ele.parentNode.offsetTop + 3 - ele.getBoundingClientRect().height;
 }
