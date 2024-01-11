43a971f53d348644f32c81a513caaaa9eee65b7d
travispettry <travis.pettry@appsbytap.com>
2024-01-11 06:55:08 -0500

    added bottom nav and floating action button

---
+++ AppsByTAP.BlazorFluentUI.PlayGround/AppsByTAP.BlazorFluentUI.Components/FloatingActionButton/FloatingActionButton.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace AppsByTAP.BlazorFluentUI.Components.FloatingActionButton
+{
+    public partial class FloatingActionButton
+    {
+        [Parameter]
+        public bool BottomNavigationIsVisible { get; set; } = false;
+        [Parameter]
+        public string ImageUrl { get; set; } = "";
+        [Parameter]
+        public string Class { get; set; }
+        [Parameter]
+        public string Style { get; set; }
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClick { get; set; }
+        [Parameter]
+        public string ID { get; set; }
+        [Parameter]
+        public bool OnClickStopPropagation { get; set; } = true;
+    }
+}
